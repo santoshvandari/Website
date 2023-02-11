13ab77f3d994b8d2dea3100781c04bcffb5a479c
Santosh Bhandari <santoshvandari1@gmail.com>
2023-02-11 22:17:36 +0545

    Personal Website

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict"
+// Auto typing text 
+let typing =  new Typed("#type",{
+    strings:["Web Developer","Wordpress Developer","Video Editor","Content Writer","Graphic Designer","Learner"],
+    typeSpeed: 120,
+    backSpeed: 120,
+    loop: true
+})
